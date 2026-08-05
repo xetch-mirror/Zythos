e7e6ca48001c073385b2e690377b3e33f3f99d26
Xetch <babyroselegaspi01@gmail.com>
2026-08-05 12:13:19 +0800

    Create timesubsys.h

---
+++ kernel/include/timesubsys.h
@@ -0,0 +1,19 @@
+#ifndef TIMESUBSYS_H
+#define TIMESUBSYS_H
+
+// subsystem identifier structure
+typedef struct {
+    const char *tag; // e.g., "serio", "rtc_cmos", "clocksource"
+} ksubsys_t;
+
+// pre-declared common kernel subsystems
+#define DEFINE_SUBSYS(name, tag_str) static const ksubsys_t name = { tag_str }
+
+DEFINE_SUBSYS(LOG_SERIO, "serio");
+DEFINE_SUBSYS(LOG_INPUT, "input");
+DEFINE_SUBSYS(LOG_RTC,   "rtc_cmos");
+DEFINE_SUBSYS(LOG_CLOCK, "clocksource");
+DEFINE_SUBSYS(LOG_NET,   "NET");
+DEFINE_SUBSYS(LOG_DISK,  "disk");
+
+#endif // KSUBSYS_H
