a9f2ad287170651d275e18ea2a370043b7b3c8b9
Xetch <babyroselegaspi01@gmail.com>
2026-08-14 15:58:53 +0800

    Update cmdtable.h

--- userland/cmdtable.h
+++ userland/cmdtable.h
@@ -1,7 +1,16 @@
-// cmdtable.h
-typedef struct { const char *name; int (*fn)(int argc, char **argv); } cmd_entry_t;
+#ifndef CMDTABLE_H
+#define CMDTABLE_H
+
+typedef struct {
+    const char *name;
+    int (*fn)(int argc, char **argv);
+} cmd_entry_t;
 
 #define REGISTER_CMD(cname, cfn) \
-    static int cfn(int argc, char **argv); \
     __attribute__((section("cmdtable"), used)) \
     static cmd_entry_t _cmd_##cfn = { cname, cfn };
+
+extern cmd_entry_t __start_cmdtable[];
+extern cmd_entry_t __stop_cmdtable[];
+
+#endif
